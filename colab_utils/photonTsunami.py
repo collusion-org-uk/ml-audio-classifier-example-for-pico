b29a6f099da00aa6d13fa9b9a75ce92fc0b420ce
richhall <richhall@users.noreply.github.com>
2023-08-16 09:48:29 +0100

    Update photonTsunami.py

--- colab_utils/photonTsunami.py
+++ colab_utils/photonTsunami.py
@@ -121,7 +121,7 @@ def record_wav_file(folder):
               }
 			  message = decoder.decode(value, { stream: true });
 			  decodedMessage += message;
-			  term.write(typeof(decodedMessage));
+			  String(decodedMessage).split('\n');;
 
 			  if (message === "start"){
 				term.write("yes - start");
